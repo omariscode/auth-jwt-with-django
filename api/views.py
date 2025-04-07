558d78b7240151c820695c65cd92014a1ae261bb
omariscode <omarscode007@gmail.com>
2025-04-07 04:10:36 +0100

    Acrescentei apenas o rate limiting para limitar o número de requisições

--- api/views.py
+++ api/views.py
@@ -1,12 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.throttling import UserRateThrottle
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 # Create your views here.
 class Home(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+    throttle_classes = [UserRateThrottle] 
 
     def get(self, request):
         message = {'message': 'Authenticated with sucess'}
